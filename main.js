e99d8d6796ad237ba6da7fdc6e149cd262574a43
Kaveri <77133605+kavericodes@users.noreply.github.com>
2021-05-10 19:50:34 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 canvas = document.getElementById("myCanvas");
 ctx = canvas.getContext("2d");
 
-car1_width = 120;
-car1_height = 70;
+car1_width = 100;
+car1_height = 100;
 car1_image = "car1.png";
 car1_x = 10;
 car1_y = 10;
 
-car2_width = 120;
-car2_height = 70;
-car2_image = "car2.png";
+car2_width = 100;
+car2_height = 100;
+car2_image = "bigcar.jpg";
 car2_x = 10;
 car2_y = 100;
 
@@ -75,4 +75,88 @@ function my_keydown(e) {
         car2_right();
         console.log("key d");
     }
+    if(car1_x > 700){
+        console.log("car1 won!");
+        document.getElementById('game_status').innerHTML = "Car 1 Won!"
+    }
+    
+    if(car2_x > 700){
+        console.log("car2 won!");
+        document.getElementById('game_status').innerHTML = "Car 2 Won!"
+    }
 }
+
+function car1_up() {
+    if(car1_y >= 0) {
+        car1_y = car1_y - 10;
+        console.log("car1_y = " + car1_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+function car1_down() {
+    if(car1_y <= 500){
+        car1_y = car1_y + 10;
+        console.log("car1_y = " + car1_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+function car1_left() {
+    if(car1_x >= 0){
+        car1_x = car1_x - 10;
+        console.log("car1_x = " + car1_x);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+function car1_right() {
+    if(car1_x <= 700){
+        car1_x = car1_x + 10;
+        console.log("car1_x = " + car1_x);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+function car2_up() {
+    if(car2_y >= 0){
+        car2_y = car2_y - 10;
+        console.log("car2_y =" + car2_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+function car2_down() {
+    if(car2_y <= 500){
+        car2_y = car2_y + 10;
+        console.log("car2_y =" + car2_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+function car2_left() {
+    if(car2_x >= 0){
+        car2_x = car2_x - 10;
+        console.log("car2_x =" + car2_x);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+function car2_right() {
+    if(car2_x <= 700){
+        car2_x = car2_x + 10;
+        console.log("car2_x =" + car2_x);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+
